f8e82eda3671afcf54df4666d5e31e85f8581eb4
Shruti Tiwari <sh03shruti@gmail.com>
2023-07-24 07:45:01 +0530

    project files

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+  cognito: {
+    userPoolId: "ap-south-1_5mvUZIndJ", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "7p7t1hbhgpsvsrjn8v49i2ddqr", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "ap-south-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "https://xc1ke9i9t2.execute-api.ap-south-1.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
+};
